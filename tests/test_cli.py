92cd17b4037c0e09f4c56a0a11bcee29648f5adf
eecavanna <eecavanna@users.noreply.github.com>
2025-09-14 18:00:04 -0700

    Update tests to check `result.output` (combination of STDOUT and STDERR)

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -11,19 +11,16 @@ class TestCLI:
     def setup_method(self):
         # Set up test runner.
         #
-        # Note: We set `mix_stderr=False` so that `result.stderr` gets populated with whatever would be sent to STDERR.
-        #
         # References:
         # - https://docs.pytest.org/en/stable/how-to/xunit_setup.html#method-and-function-level-setup-teardown
         # - https://typer.tiangolo.com/tutorial/testing/#import-and-create-a-clirunner
-        # - https://github.com/fastapi/typer/issues/385 (RE: using `mix_stderr=False`)
         #
-        self.runner = CliRunner(mix_stderr=False)
+        self.runner = CliRunner()
 
     def test_version_option(self):
         result = self.runner.invoke(app, ["--version"])
         assert result.exit_code == 0
-        output_str = result.stdout
+        output_str = result.output
 
         # Assert that the version string contains (at least somewhere within it)
         # "digit(s), dot, digit(s), dot, digit(s)" (e.g. "1.2.3"). This allows for
@@ -33,35 +30,35 @@ class TestCLI:
     def test_help_option(self):
         result = self.runner.invoke(app, ["--help"])
         assert result.exit_code == 0
-        assert "Usage:" in result.stdout
+        assert "Usage:" in result.output
 
     def test_specifying_json_via_argument(self):
         json_str = r'{"name": "Ryu", "age": 25}'
         result = self.runner.invoke(app, [json_str])
         assert result.exit_code == 0
-        assert result.stdout == "$.age,$.name\n25,Ryu\n"
+        assert result.output == "$.age,$.name\n25,Ryu\n"
 
     def test_specifying_json_via_stdin(self):
         json_str = r'{"name": "Ryu", "age": 25}'
         result = self.runner.invoke(app, input=json_str)
         assert result.exit_code == 0
-        assert result.stdout == "$.age,$.name\n25,Ryu\n"
+        assert result.output == "$.age,$.name\n25,Ryu\n"
 
     def test_specifying_invalid_json(self):
         invalid_json_string = r'{"name": "Ryu",, "age": 25}'
         result = self.runner.invoke(app, [invalid_json_string])
         assert result.exit_code == 1
-        assert "Invalid JSON string" in result.stderr
+        assert "Invalid JSON string" in result.output
 
         result = self.runner.invoke(app, input=invalid_json_string)
         assert result.exit_code == 1
-        assert "Invalid JSON string" in result.stderr
+        assert "Invalid JSON string" in result.output
 
     def test_specifying_empty_string(self):
         result = self.runner.invoke(app, [r""])
         assert result.exit_code == 1
-        assert "Invalid JSON string" in result.stderr
+        assert "Invalid JSON string" in result.output
 
         result = self.runner.invoke(app, input=r"")
         assert result.exit_code == 1
-        assert "No JSON was provided via STDIN" in result.stderr
+        assert "No JSON was provided via STDIN" in result.output
